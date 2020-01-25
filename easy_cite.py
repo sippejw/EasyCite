41bf4dad33068c6017ea3992c75dc165e7b434cc
Jacob Justice <jacobjustice@gmail.com>
2020-01-25 13:41:23 -0500

    added function for google OCR

---
+++ easy_cite.py
@@ -0,0 +1,31 @@
+from google.cloud import vision
+import io
+#image similarity
+
+def textbook_recognition():
+    pass
+
+#google vision api
+def quotation_maker():
+    path = './math1.jpg'
+
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+
+quotation_maker()
